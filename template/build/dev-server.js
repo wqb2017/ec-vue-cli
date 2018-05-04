818d2f9a93489e484120d40c0ead9e65a37e7004
wqb2017 <wqb2017@yeah.net>
2018-05-04 18:05:49 +0800

    启动本机ip

--- template/build/dev-server.js
+++ template/build/dev-server.js
@@ -1,16 +1,17 @@
-require("./check-versions")();
-var config = require("../config");
+var ip = require('ip');
+require('./check-versions')();
+var config = require('../config');
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV);
 }
-require("./write-get-router-methods");
+require('./write-get-router-methods');
 
-var opn = require("opn");
-var path = require("path");
-var express = require("express");
-var webpack = require("webpack");
-var proxyMiddleware = require("http-proxy-middleware");
-var webpackConfig = require("./webpack.dev.conf");
+var opn = require('opn');
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var proxyMiddleware = require('http-proxy-middleware');
+var webpackConfig = require('./webpack.dev.conf');
 
 // default port where dev server listens for incoming traffic
 var port = process.env.PORT || config.dev.port;
@@ -23,19 +24,19 @@ var proxyTable = config.dev.proxyTable;
 var app = express();
 var compiler = webpack(webpackConfig);
 
-var devMiddleware = require("webpack-dev-middleware")(compiler, {
+var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   quiet: true,
 });
 
-var hotMiddleware = require("webpack-hot-middleware")(compiler, {
+var hotMiddleware = require('webpack-hot-middleware')(compiler, {
   log: () => {},
   heartbeat: 2000,
 });
 // force page reload when html-webpack-plugin template changes
-compiler.plugin("compilation", function(compilation) {
-  compilation.plugin("html-webpack-plugin-after-emit", function(data, cb) {
-    hotMiddleware.publish({ action: "reload" });
+compiler.plugin('compilation', function(compilation) {
+  compilation.plugin('html-webpack-plugin-after-emit', function(data, cb) {
+    hotMiddleware.publish({ action: 'reload' });
     cb();
   });
 });
@@ -43,14 +44,14 @@ compiler.plugin("compilation", function(compilation) {
 // proxy api requests
 Object.keys(proxyTable).forEach(function(context) {
   var options = proxyTable[context];
-  if (typeof options === "string") {
+  if (typeof options === 'string') {
     options = { target: options };
   }
   app.use(proxyMiddleware(options.filter || context, options));
 });
 
 // handle fallback for HTML5 history API
-app.use(require("connect-history-api-fallback")());
+app.use(require('connect-history-api-fallback')());
 
 // serve webpack bundle output
 app.use(devMiddleware);
@@ -61,20 +62,20 @@ app.use(hotMiddleware);
 
 // serve pure static assets
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory);
-app.use(staticPath, express.static("./static"));
-app.use("/libs", express.static("./libs"));
-var uri = "http://localhost:" + port;
+app.use(staticPath, express.static('./static'));
+app.use('/libs', express.static('./libs'));
+var uri = 'http://' + ip.address() + ':' + port;
 
 var _resolve;
 var readyPromise = new Promise(resolve => {
   _resolve = resolve;
 });
 
-console.log("> Starting dev server...");
+console.log('> Starting dev server...');
 devMiddleware.waitUntilValid(() => {
-  console.log("> Listening at " + uri + "\n");
+  console.log('> Listening at ' + uri + '\n');
   // when env is testing, don't need open it
-  if (autoOpenBrowser && process.env.NODE_ENV !== "testing") {
+  if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri);
   }
   _resolve();
