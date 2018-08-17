f2d5907d433b52594343948e392c303c7c6c3826
wqb2017 <542672187@qq.com>
2018-08-17 10:44:56 +0800

    新增css reset

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -6,8 +6,6 @@ module.exports = {
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
     'autoprefixer': {
-    },
-    'postcss-pxtorem': {
     }
   }
 }
